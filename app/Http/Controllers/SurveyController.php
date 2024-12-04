e8f0e961556f4e6fd5f43676d6a6facc04121b4b
miraz66 <mirajulislam0509@gmail.com>
2024-12-05 01:38:31 +0600

    Add model survey and Question

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -168,4 +168,6 @@ function createQuestion($data)
         'data' => 'required',
         'description' => 'nullable | string',
     ]);
+
+    return SurveyQuestion::create($validator->validated());
 }
