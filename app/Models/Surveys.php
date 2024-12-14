545d8bb22fa143e8422153c5d440098ac1d501a6
miraz66 <mirajulislam0509@gmail.com>
2024-12-14 11:36:27 +0600

    Eidt This Survey

--- app/Models/Surveys.php
+++ app/Models/Surveys.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Surveys extends Model
 {
-    //
+    use HasFactory, Notifiable, HasApiTokens;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+        'status',
+        'slug',
+        'expire_at',
+        'image',
+        'type',
+        'questions'
+    ];
 }
